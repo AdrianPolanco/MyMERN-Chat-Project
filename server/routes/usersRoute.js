a480aa6ebd53892f241e1c71e38bfbdabc3e810a
Adrian <adferrer0611@gmail.com>
2023-06-03 01:44:38 -0400

    Endpoints iniciales

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -1,2 +1,63 @@
-const user = require("../models/userModel");
-const express = require("express").Router();
+const User = require("../models/userModel");
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+//User registration
+router.post("/register", async (req, res) => {
+    try {
+        //Checking if there is any user with the same email than the one that is trying to register
+        const user = User.findOne({ email: req.body.email });
+        if (user) {
+            return res.send({ message: "User already exists", success: false });
+        }
+
+        //Encrypting password
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        //Giving the encrypted password to the password from the req.body
+        req.body.password = hashedPassword;
+        //Creating the new user
+        const NewUser = new User(req.body);
+        //Saving the document in the database
+        await NewUser.save();
+
+        //Sending response to frontend
+        res.send({ message: "User created successfully", success: true });
+    } catch (error) {
+        res.send({ message: error.message, success: false });
+    }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        //Checking if there is such user in the database
+        const user = await User.findOne(req.body.email);
+        if (!user) {
+            return res.send({
+                message: "The user does not exist",
+                success: false,
+            });
+        }
+
+        //Checking the passarowds
+
+        const validPassword = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
+        if (!validPassword) {
+            return res.send({ message: "Invalid password", success: false });
+        }
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_TOKEN);
+        res.send({
+            message: "User logged succesfully",
+            success: false,
+            Data: token,
+        });
+    } catch (error) {
+        res.send({ message: error.message, success: false });
+    }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,5 +3,9 @@ const express = require("express");
 const app = express();
 const dbConfig = require("./config/dbconfig");
 const port = process.env.PORT || 3000;
+const userRoute = require("./routes/usersRoute");
+
+app.use(express.json);
+app.use("/api/users", userRoute);
 
 app.listen(port, () => `Server running on the port ${port}`);
