39d6c03dbba3f6e906e0fbaa8fc5a202b2d0ab72
Adrian <adferrer0611@gmail.com>
2023-06-16 18:20:39 -0400

    Detalles del login

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { GetCurrentUser } from "../apicalls/users";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 
 const ProtectedRoute = ({ children }) => {
     const navigate = useNavigate();
@@ -11,8 +12,18 @@ const ProtectedRoute = ({ children }) => {
                 //children.props.username = response.Data.email;
                 return true;
             } else {
+                if (!toast.isActive(toast.toastId)) {
+                    toast.error(
+                        response.message.charAt(0).toUpperCase() +
+                            response.message.slice(1),
+                        {
+                            position: toast.POSITION.TOP_CENTER,
+                            toastId: "TokenError",
+                            delay: 0,
+                        }
+                    );
+                }
                 navigate("/login");
-
                 return false;
             }
         } catch (error) {
